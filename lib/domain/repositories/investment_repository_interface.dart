69f18ecf52e65a22e6bf226754a1a94ac2941580
Reeze <azfaalharits25@gmail.com>
2025-06-17 09:44:43 +0700

    chore: update structure

--- lib/domain/repositories/investment_repository_interface.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/investment_entity.dart';
-
-abstract class InvestmentRepositoryInterface {
-  Future<List<InvestmentEntity>> getInvestments();
-  Future<InvestmentEntity> getInvestmentById(String id);
-  Future<InvestmentEntity> createInvestment(InvestmentEntity investment);
-  Future<InvestmentEntity> updateInvestment(InvestmentEntity investment);
-  Future<void> deleteInvestment(String id);
-}
--- lib/domain/repositories/user_repository_interface.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/user_entity.dart';
-
-abstract class UserRepositoryInterface {
-  Future<UserEntity?> getCurrentUser();
-  Future<UserEntity> login(String email, String password);
-  Future<void> logout();
-  Future<void> updateProfile(UserEntity user);
-}
